9b70b632cca1cd7bef1fad228c4e385119273ac9
kfir252 <43969860+kfir252@users.noreply.github.com>
2024-07-12 08:12:33 +0300

    made a dbconnection better, with chack

--- dbConnection.py
+++ dbConnection.py
@@ -6,7 +6,6 @@ import uuid
 import datetime
 import os
 
-
 Base = declarative_base()
 
 class User(Base):
@@ -22,62 +21,58 @@ class User(Base):
     def __repr__(self):
         return f"({self.id},{self.email})"
     
-    # @staticmethod
-    # def getUser(email):
-    #     ans = session.query(User).filter(User.email == email)
-    #     a = None
-    #     for a in ans:
-    #         return a
-        
+    @staticmethod
+    def getUser(email):
+        ans = session.query(User).filter(User.email == email)
+        a = None
+        for a in ans:
+            return a
+
 class Upload(Base):
     __tablename__ = 'uploads'
     
     id = Column(Integer, primary_key=True)
-    uid = Column(String, nullable=False, primary_key=True)
+    uid = Column(String, nullable=False)
     filename = Column(String, nullable=False)
     upload_time = Column(DateTime)
     finish_time = Column(DateTime, nullable=True)
     status = Column(String)
-    json = Column(String)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=True)
-    path = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     user = relationship('User', back_populates='uploads')
 
-    def __init__(self, filename, user, path):
+    def __init__(self, filename, email):
         self.uid = str(uuid.uuid4())
         self.filename = filename
         self.upload_time = datetime.datetime.utcnow()
         self.finish_time = None
-        self.path = path
-        self.status = 'pending'
-        self.user = user
-        
-        self.user_id = None
-        if user != None:
-            self.user_id = user.id
+        self.status = "panding"
+        self.user = User.getUser(email)
+        self.user_id = self.user.id
         
     def __repr__(self):
         return f"({self.id},{self.uid},{self.filename},{self.upload_time},{self.finish_time},{self.status},{self.user_id},{self.user})"
 
-    # @staticmethod
-    # def getJsonPath(uid):
-    #     ans = session.query(Upload).filter(Upload.uid == uid)
-    #     a = None
-    #     for a in ans:
-    #         return a.path
-            
 engine = create_engine("sqlite:///db/app.db", echo=True)
 Base.metadata.create_all(bind=engine)
+
 Session = sessionmaker(bind=engine)
 session = Session()
 
 
-
 if __name__ == "__main__":
-    us = User("test@gmail.com")
-    up = Upload('file.pptx',us, 'abc/file.pptx')
-         
-    session.add(us)
-    session.add(up)
+    #Make User
+    session.add(User("test@gmail.com"))
     
+    #Make Uploads
+    session.add(Upload('first', "test@gmail.com"))
+    session.add(Upload('sec', "test@gmail.com"))
+    session.add(Upload('שלישי', "test@gmail.com"))
+    session.add(Upload('רביעי', "test@gmail.com"))
+    
+    #Check On Users Uploads
+    for i in User.getUser("test@gmail.com").uploads:
+        print(i)
+    
+    #commit changes
     session.commit()
+    
